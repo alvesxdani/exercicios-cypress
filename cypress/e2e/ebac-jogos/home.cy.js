ec18c076399c49722e870e99a3e8e42cd708e3b9
alvesxdani <dani.alvesm22@gmail.com>
2024-05-06 00:34:44 -0300

    :tada: Initial commit

---
+++ cypress/e2e/ebac-jogos/home.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+describe('Testes para a home', () => {
+  it('Deve renderezizar 4 vagas', () => {
+    cy.visit('https://ebac-jobs-e2e.vercel.app/')
+    cy.get('.ListaVagas_vagas__gmNZn > li').should('have.length', 4)
+  })
+  it('Deve filtrar por fullstack', () => {
+    cy.visit('https://ebac-jobs-e2e.vercel.app/')
+    cy.get('.FormVagas_campo__E1ppF').type('fullstack{enter}')
+    cy.get('.ListaVagas_vagas__gmNZn > li').should('have.length', 1).should('contain', 'fullstack')
+  })
+})
